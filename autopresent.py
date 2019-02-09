9b3090bb6ce7fbc93d013453a67608cc9f5b59d0
Pablo Brasero <pablo@pablobm.com>
2019-02-09 13:46:33 +0000

    Misc changes while in the field

--- autopresent.py
+++ autopresent.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import re
 import subprocess
 
@@ -7,7 +8,11 @@ class DisplayInfo:
 
     @property
     def primary(self):
-        return next(d for d in self.displays if d.is_primary)
+        try:
+            return next(d for d in self.displays if d.is_primary)
+        except StopIteration:
+            # No screeen marked as primary
+            None
 
     @property
     def secondaries(self):
@@ -37,7 +42,9 @@ class Mode:
         self.h = h
 
     def correction_for(self, other):
-        return CorrectionFactor(self.w / other.w, self.h / other.h)
+        print self
+        print other
+        return CorrectionFactor(float(self.w) / other.w, float(self.h) / other.h)
 
     def __str__(self):
         return '%sx%s' % (self.w, self.h)
@@ -53,30 +60,38 @@ class CorrectionFactor:
 
 def run(line):
     print(line)
-    subprocess.run(line.split(" "))
+    proc = subprocess.Popen(line.split(" "), stdout=subprocess.PIPE)
+    return proc.communicate()[0]
 
 displays = []
-result = subprocess.run(['xrandr'], capture_output=True)
-for line in result.stdout.decode('utf8').splitlines():
+result = run('xrandr')
+for line in result.decode('utf8').splitlines():
     m = re.search(r'^(\S+) connected (primary)?', line)
     if m:
         display = Display()
-        display.name = m[1]
-        display.is_primary = bool(m[2])
+        display.name = m.group(1)
+        display.is_primary = bool(m.group(2))
         displays.append(display)
     else:
         m = re.search(r'^\s+(\d+)x(\d+) ', line)
         if m:
-            mode = Mode(int(m[1]), int(m[2]))
+            mode = Mode(int(m.group(1)), int(m.group(2)))
             display.modes.append(mode)
 
 dinfo = DisplayInfo(displays)
 primary = dinfo.primary
+if not primary:
+    try:
+        heuristic_primary = next(d for d in dinfo.displays if d.name == 'LVDS1')
+    except StopIteration:
+        raise Exception
+    primary = heuristic_primary
+    primary.is_primary = True
+
 pmode = primary.max_mode
 
 run('xrandr --output %s --mode %s --pos 0x0' % (primary.name, pmode))
 for d in dinfo.secondaries:
     smode = d.max_mode
     correction = pmode.correction_for(smode)
-    print(correction)
     run('xrandr --output %s --pos 0x0 --mode %s --scale %s' % (d.name, smode, correction))
